8016bc148e3a01e8a25e654b7fba926be34c58b4
joyu8 <142926471+joyu8@users.noreply.github.com>
2024-01-23 09:41:26 -0800

    Update PigLatin.java

--- src/main/java/PigLatin.java
+++ src/main/java/PigLatin.java
@@ -27,22 +27,36 @@ public class PigLatin {
 	        System.out.println(pigLatin(lines[i]));
 	    }
     }
-    public int findFirstVowel(String sWord) {
-        //precondition: sWord is a valid String of length greater than 0.
-        //postcondition: returns the position of the first vowel in sWord.  If there are no vowels, returns -1
-	    // your code goes here
-        return -1;
-    }
+    public int findFirstVowel(String sWord)
+//precondition: sWord is a valid String of length greater than 0.
+//postcondition: returns the position of the first vowel in sWord.  If there are no vowels, returns -1
+{
+  if(sWord.substring(0, 1).equals("a")||sWord.substring(0, 1).equals("e") 
+  ||sWord.substring(0, 1).equals("i") ||sWord.substring(0, 1).equals("o") 
+  ||sWord.substring(0, 1).equals("u")){
+    return -1;
+  }
+  for(int i = 0; i < sWord.length()-1; i++){
+     if(sWord.substring(i, i+1).equals("a")||sWord.substring(i, i+1).equals("e") 
+  ||sWord.substring(i, i+1).equals("i") ||sWord.substring(i, i+1).equals("o")
+  ||sWord.substring(i, i+1).equals("u")){
+      return 0;
+  }
 
-    public String pigLatin(String sWord) {
-        //precondition: sWord is a valid String of length greater than 0
-        //postcondition: returns the pig latin equivalent of sWord
-        // more code should go here
-	    if(findFirstVowel(sWord) == -1) {
-		    return sWord + "ay";
-	    }
-	    else {
-		return "ERROR!";
-	    }
-    }
-}//end PigLatin class
+  
+  }
+  return 2;
+  }
+
+public String pigLatin(String sWord)
+//precondition: sWord is a valid String of length greater than 0
+//postcondition: returns the pig latin equivalent of sWord
+{
+  if(findFirstVowel(sWord) == -1)
+    return sWord + "ay";
+  if(findFirstVowel(sWord) == 2)
+    return(sWord.substring(0) + "ay");
+  if(findFirstVowel(sWord) == 0)
+    return(sWord.substring(1, sWord.length()) + sWord.substring(0, 1) + "ay");
+  return "a";
+}
